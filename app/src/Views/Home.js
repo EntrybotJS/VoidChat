602c916c4d88dd9da0a57f01bf197c84aed4c61f
EntrybotJS <entrybotjs@gmail.com>
2018-04-23 07:38:30 -0400

    Started homepage ui

--- app/src/Views/Home.js
+++ app/src/Views/Home.js
@@ -1,15 +1,41 @@
 //	eslint-disable-next-line no-unused-vars
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/Home.css'
+import PropTypes from 'prop-types'
 
 class Home extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+
+
+	}
 	render() {
 		return (
-			<div className="Home">
-				<p>Hello World!</p>
-			</div>
+			<main>
+				<div className='page'>
+					<div className='landing'>
+						<div className='menu'>
+							<h1>{this.props.brand}</h1>
+							<Link to='/privacy'>Privacy</Link>
+							<Link to='/how'></Link>
+						</div>
+						<div className=''>
+
+						</div>
+					</div>
+					<div className='info'>
+
+					</div>
+				</div>
+			</main>
 		)
 	}
+}
+
+Home.propTypes = {
+	brand: PropTypes.string.isRequired
 }
 
 export default Home
--- app/src/index.js
+++ app/src/index.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './styles/index.css'
-import Home from './Views/Home'
+import Home from './views/Home'
 import registerServiceWorker from './registerServiceWorker'
 
-class App extends React.Component {
-	render() {
-		return (
-			<Router>
-				<Route exact path="/" component={Home}/>
-			</Router>
-		)
-	}
-}
+const App = () => (
+	<Router>
+		<Switch>
+			<Route exact path="/" render={(props) => (<Home {... props} brand='VoidChat'/>)}/>
+			<Route render={() => (<p>Oops, it seems like you got lost.</p>)}/>
+		</Switch>
+	</Router>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
