d8460a356c4fe9884620adea29b36f67aa8ef774
entrybotjs <entrybotjs@gmail.com>
2018-05-05 02:58:47 -0400

    Finished login form

---
+++ app/src/components/loginForm.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+class LoginForm extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			handle: '',
+			password: ''
+		}
+
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+	handleSubmit(e) {
+		e.preventDefault()
+		
+		let code = verifyCreds(e.target.handle.value, e.target.password.value)
+
+		if(code === 0) { //	Send request to api
+			this.props.onFormSuccess(e.target.handle.value, e.target.password.value)
+		} else {
+			this.props.onFormError(code)
+		}
+	}
+	handleChange(event) {
+		const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
+		this.setState({[event.target.name]: value})
+	}
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input type='text' name='handle' placeholder='username' onChange={this.handleChange}/>
+				<input type='password' name='password' placeholder='password' onChange={this.handleChange}/>
+				<div className='register-link'>
+					<Link to='/register'>I do not have an account</Link>
+					<button value='submit'>Envoyer</button>
+				</div>
+			</form>
+		)
+	}
+}
+
+LoginForm.propTypes = {
+	onFormError: PropTypes.func.isRequired,
+	onFormSuccess: PropTypes.func.isRequired
+}
+
+//	eslint-disable-next-line
+function verifyCreds(handle, password) {
+	if(handle === '' || handle == null) {
+		return 1
+	}
+
+	if(password === '' || password == null) {
+		return 2
+	}
+
+	return 0
+}
+
+export default LoginForm
--- app/src/views/home.js
+++ app/src/views/home.js
@@ -1,8 +1,8 @@
 //	eslint-disable-next-line no-unused-vars
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import '../styles/home.css'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import LoginForm from '../components/loginForm'
 import axios from 'axios'
 
 class Home extends Component {
@@ -50,7 +50,6 @@ class Home extends Component {
 							<h1>Login</h1>
 							<LoginError error={this.state.error}/>
 							<LoginForm onFormError={this.handleFormError} onFormSuccess={this.handleFormSuccess}/>
-							<Link to='/register'>I do not have an account</Link>
 						</div>
 					</div>
 					<div className='info'>
@@ -66,71 +65,11 @@ Home.propTypes = {
 	brand: PropTypes.string.isRequired
 }
 
-class LoginForm extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			handle: '',
-			password: ''
-		}
-
-		this.handleSubmit = this.handleSubmit.bind(this)
-		this.handleChange = this.handleChange.bind(this)
-	}
-	handleSubmit(e) {
-		e.preventDefault()
-		
-		let code = verifyCreds(e.target.handle.value, e.target.password.value)
-
-		if(code === 0) { //	Send request to api
-			this.props.onFormSuccess(e.target.handle.value, e.target.password.value)
-		} else {
-			this.props.onFormError(code)
-		}
-	}
-	handleChange(event) {
-		const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
-		this.setState({[event.target.name]: value})
-	}
-	render() {
-		return (
-			<form onSubmit={this.handleSubmit}>
-				<div className='input-label'>
-					<input type='text' name='handle' onChange={this.handleChange}/>
-					<label>handle</label>
-				</div>
-				<div className='input-label'>
-					<input type='password' name='password' onChange={this.handleChange}/>
-					<label>password</label>
-				</div>
-				<input type='submit'/>
-			</form>
-		)
-	}
-}
-
-LoginForm.propTypes = {
-	onFormError: PropTypes.func.isRequired,
-	onFormSuccess: PropTypes.func.isRequired
-}
-
 const LoginError = (props) => (
 	<p className='login-error'>{(props.error) ? props.error : null}</p>
 )
 
-//	eslint-disable-next-line
-function verifyCreds(handle, password) {
-	if(handle === '' || handle == null) {
-		return 1
-	}
-
-	if(password === '' || password == null) {
-		return 2
-	}
-
-	return 0
-}
-
+//	This should be integrated in the loginForm conponent
 function translateError(errorCode) {
 	switch(errorCode) {
 	case 1:
